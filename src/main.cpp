a6ffab5831053ff8e7d6acbfbb108708cd59e0e4
kraxarn <me@kraxarn.com>
2020-07-27 23:17:28 +0200

    More QML stuff

--- src/main.cpp
+++ src/main.cpp
@@ -5,14 +5,38 @@
 #include <QApplication>
 #include <QCoreApplication>
 
+#define USE_QT_QUICK
+
+#ifdef USE_QT_QUICK
+#include "qml/spotifyqml.hpp"
+
+#include <QQmlApplicationEngine>
+#include <QQmlContext>
+#include <QQuickStyle>
+#endif
+
+#ifdef USE_QT_QUICK
+void defineTypes(QQmlApplicationEngine &engine)
+{
+	engine.rootContext()->setContextProperty("AppVersion", APP_VERSION);
+
+	qmlRegisterType<SpotifyQml>(
+		"com.kraxarn.spotify",
+		1, 0,
+		"Spotify");
+}
+#endif
+
 int main(int argc, char *argv[])
 {
 	// Set name for settings etc.
 	QCoreApplication::setOrganizationName("kraxarn");
 	QCoreApplication::setApplicationName("spotify-qt");
 	QCoreApplication::setApplicationVersion(APP_VERSION);
+
 	// Create Qt application
 	QApplication app(argc, argv);
+
 	// JSON Settings
 	Settings settings;
 	if (!QFile::exists(Settings::fileName()))
@@ -26,12 +50,15 @@ int main(int argc, char *argv[])
 		jsonFile.close();
 		settings.load();
 	}
+
 	// Check fallback icons
 	Icon::useFallbackIcons = settings.general.fallbackIcons;
+
 	// Show version if requested
 	QCommandLineParser parser;
 	parser.addVersionOption();
 	parser.process(app);
+
 	// First setup window
 	if (settings.account.refreshToken.isEmpty())
 	{
@@ -39,9 +66,22 @@ int main(int argc, char *argv[])
 		if (dialog.exec() == QDialog::Rejected)
 			return 0;
 	}
-	// Create main window
+
+#ifdef USE_QT_QUICK
+	QQmlApplicationEngine engine;
+	QQuickStyle::setStyle(
+		QQuickStyle::availableStyles().contains("Plasma")
+		? "Plasma"
+		: "Material/Dark");
+	qDebug() << "using" << QQuickStyle::name() << "style";
+	defineTypes(engine);
+	engine.load(QUrl("qrc:/qml/main.qml"));
+#else
+	// Create and show main window
 	MainWindow w(settings);
-	// Show window and run application
 	w.show();
+#endif
+
+	// Run application
 	return QApplication::exec();
 }
